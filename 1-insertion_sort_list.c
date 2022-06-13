642f18da37f37194ae1d743200406961c564dd94
Joanfa7 <jafabre95@gmail.com>
2022-06-13 16:00:08 +0000

    adding new file

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list- sorts a dobly linked list of integers in ascending order
+ * @list: list to be sorted
+ * Return: success
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *NextPtr, *PrevPtr, *Traverse;
+
+	if (list == NULL || (*list)->next == NULL)
+		return;
+
+	for (NextPtr = (*list); NextPtr != NULL; NextPtr = Traverse)
+	{
+		Traverse = NextPtr->next;
+		PrevPtr = NextPtr->prev;
+
+		while (PrevPtr != NULL && NextPtr->n < PrevPtr->n)
+		{
+			swap(list, &PrevPtr, NextPtr);
+			print_list((const listint_t *)*list);
+		}
+	}
+}
+
+void swap(listint_t **head, listint_t **left, listint_t *right)
+{
+	(*left)->next = right->next;
+	if (right->next != NULL)
+		right->next->prev = *left;
+
+	right->prev = (*left)->prev;
+	right->prev = (*left)->prev;
+	right->next = *left;
+
+	if ((*left)->prev != NULL)
+	{
+		(*left)->prev->next = right;
+	}
+	else
+		*head = right;
+	(*left)->prev = right;
+	*left = right->prev;
+}
